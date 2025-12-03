c2c4c972f0c3a76151af4bc29e345779b7eee4d5
OmerEnsarshn <250542022@firat.edu.tr>
2025-12-03 21:37:56 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,10 +11,15 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        // Cevap 1: Dönen değer kullanılmasa bile Java çalıştırır, derleyici hata vermez. Sadece IDE uyarısı olabilir.
+
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+
+        // Cevap 2: Void metod bir değer döndürmediği için ifade içinde kullanılamaz. Derleme hatası verir.
+
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
